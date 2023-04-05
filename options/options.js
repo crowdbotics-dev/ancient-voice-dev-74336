d3e9614dd15563150e48d071aa56b6c6d8d07287
Crowdbotics <engineering@crowdbotics.com>
2023-04-05 00:17:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-voice-dev-74336",
+  url: "https://ancient-voice-dev-74336.botics.co",
+  api: "https://ancient-voice-dev-74336.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
